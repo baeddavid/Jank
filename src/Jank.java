58b67b383a15db8281b6defdce281d4ba78e4e09
David <baeddavid@gmail.com>
2021-11-27 17:08:42 -0600

    initialize frame and text area

--- src/Jank.java
+++ src/Jank.java
@@ -1,7 +1,28 @@
-// Main class. This class assembles our text editor.
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
+import java.awt.event.ActionListener;
 
-public class Jank {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+public class Jank extends JFrame implements ActionListener {
+    // Text component
+    static JTextArea textArea;
+
+    // Frame for the editor
+    static JFrame frame;
+
+    // Constructor
+    public Jank() {
+        frame = new JFrame("Jank");
+        // Placeholder UI
+        try {
+            // Set metal look and feel
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+
+            // Set theme to ocean
+            MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
