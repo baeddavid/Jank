8741646065c7bc3f00c253c326bf12ec21efa15b
David <baeddavid@gmail.com>
2021-11-27 21:43:11 -0600

    add remove keybind method

---
+++ src/Keybinds/RemoveKeybind.java
@@ -0,0 +1,9 @@
+package Keybinds;
+
+import javax.swing.*;
+
+public class RemoveKeybind {
+    public static void removeKeybind(JTextArea textArea, int key, int modifier) {
+        textArea.getInputMap().put(KeyStroke.getKeyStroke(key, modifier), "none");
+    }
+}
