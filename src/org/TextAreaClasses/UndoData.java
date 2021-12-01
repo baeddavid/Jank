fea0809034ae262efbfccbdfbf0ca649d8ef0cf9
David <baeddavid@gmail.com>
2021-11-30 18:31:54 -0600

    add UndoData class which store the text of the undo action

---
+++ src/org/TextAreaClasses/UndoData.java
@@ -0,0 +1,14 @@
+package org.TextAreaClasses;
+
+public class UndoData {
+
+    private String undoText;
+
+    public UndoData(String undoText) {
+        this.undoText = undoText;
+    }
+
+    public String getUndoText() {
+        return undoText;
+    }
+}
